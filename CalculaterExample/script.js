2e9a2ec48ff76344bd06dfa80489128f7d45a31b
ahmet <ahmet@ahmet-Air.home>
2024-06-03 13:56:28 +0300

    Calculater example

---
+++ CalculaterExample/script.js
@@ -0,0 +1,24 @@
+function multiply(){
+	var num1 = document.getElementById("num1").value
+	var num2 = document.getElementById("num2").value
+	var sonuc = num1 * num2;
+	alert(sonuc)
+}
+function Add(){
+	var num1 = document.getElementById("num1").value
+	var num2 = document.getElementById("num2").value
+	var sonuc = Number(num1) + Number(num2);
+	alert(sonuc)
+}
+function cikarma(){
+	var num1 = document.getElementById("num1").value
+	var num2 = document.getElementById("num2").value
+	var sonuc = Number(num1) - Number(num2);
+	alert(sonuc)
+}
+function bolme(){
+	var num1 = document.getElementById("num1").value
+	var num2 = document.getElementById("num2").value
+	var sonuc = Number(num1) / Number(num2);
+	alert(sonuc)
+}
