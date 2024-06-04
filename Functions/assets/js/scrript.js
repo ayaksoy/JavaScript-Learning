117caa72fc935d0e1c09009fd6319e401b070d9d
ahmet <yasir.aksoy@outlook.com>
2024-06-04 09:51:50 +0300

    Functions

--- Functions/assets/js/scrript.js
+++ Functions/assets/js/scrript.js
@@ -1,35 +1,42 @@
-// //yerleşik fonksuiyonlar
-// alert("Merhaba Dünya")
-// console.log("sistem kaydı")
-// //promt
-// let isim = prompt("Adınızı giriniz")
-// document.write("Merhaba " + isim)
 
-function bke() {
-    let boy = prompt("boyunuzu giriniz")
-    let kilo = prompt("kilonuzu giriniz")
+//Yerleşik fonksiyonlar
+// alert("Merhaba Js");
+// console.log("Sistem Kaydı");
+// //propmt -> Console.ReadLine()
+// let isim=prompt("Adınızı Nedir?");
+// document.write(isim);
 
-    let sonuc = kilo / (boy * boy) * 10000;
-    let hesapsonucu = document.getElementById("endexSonucu");
-    hesapsonucu.textContent = "endex sonucu" + sonuc;
+function bke(){
+    let boy=prompt("Boy giriniz (cm)");
+    let kilo=prompt("Kilo giriniz (kg)");
 
-    if (sonuc < 18.5) {
-        hesapsonucu.textContent = sonuc+ "zayıfsınız"
-        hesapsonucu.style.color = "orange"
-    } else if (sonuc >= 18.5 && sonuc < 24.9) {
-        hesapsonucu.textContent = sonuc+ " "+"normal kilodasınız"
-        hesapsonucu.style.color = "green"
-    } else if (sonuc >= 25 && sonuc < 29.9) {
-        hesapsonucu.textContent = sonuc+ "fazla kilolusunuz"
-        hesapsonucu.style.color = "orange"
-    } else if (sonuc >= 30 && sonuc < 34.9) {
-        hesapsonucu.textContent = sonuc+"1. derece obezsiniz"
-        hesapsonucu.style.color = "red"
-    } else if (sonuc >= 35 && sonuc < 44.9) {
-        hesapsonucu.textContent = sonuc+"2. derece obezsiniz"
-        hesapsonucu.style.color = "red"
-    } else {
-        hesapsonucu.textContent = sonuc+"3. derece obezsiniz"
-        hesapsonucu.style.color = "red"
+    let sonuc=kilo/(boy*boy)*10000;
+   
+    let endexSonucu=document.getElementById("endexSonucu");
+    let endexMetin=document.getElementById("endexMetin");
+    let endexImg=document.getElementById("endexImg");
+   
+    endexSonucu.textContent="Endex Sonucu: "+sonuc;
+
+    endexImg.style.width="100px";
+    if(sonuc < 18.5){
+        endexMetin.innerText="Düşük Kilolu";
+        endexMetin.style.color="pink";
+        endexImg.src="assets/images/dusuk.png";
+    }
+    else if(sonuc < 25){
+        endexMetin.innerText="Normal Kilo";
+        endexMetin.style.color="green";
+        endexImg.src="assets/images/normal.png";
+    }
+    else if(sonuc < 30){
+        endexMetin.innerText="Kilolu";
+        endexMetin.style.color="orange";
+        endexImg.src="assets/images/kilolu.png";
+    }
+    else if(sonuc >= 30){
+        endexMetin.innerText="Obez";
+        endexMetin.style.color="red";
+        endexImg.src="assets/images/obez.png";
     }
 }
